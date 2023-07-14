e66ae93843e2e25c285d38f55c788b19c4d5f21a
juan <fernan2lopezkto@gmail.com>
2023-07-14 15:22:36 -0300

    change imagen theme photos

--- src/Containers/team.js
+++ src/Containers/team.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import renzo from "../static/img/Renzo_Yaques.jpg"
-import joaco from "../static/img/Joaquin_Furtado.jpg"
-import mauri from "../static/img/Mauricio_De_Betolaza.jpg"
-import juan from "../static/img/Juan_Rey.jpg"
+import renzo from "../static/img/Renzo Yaques 4.jpg"
+import joaco from "../static/img/Joaquin Furtado 3.jpg"
+import mauri from "../static/img/Mauricio_De_Betolaza_2.jpg"
+import juan from "../static/img/juan_rey_3.jpg"
 
 import ln from "../static/icons/icon-linkedin.png"
 import gh from "../static/icons/icon-github.png"
@@ -36,10 +36,10 @@ const Team = () => {
                 <ul class="list-inline">
 
                 <li class="list-inline-item">
-                <a title="linkedin" href="http://www.linkedin.com" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="linkedin" href="https://www.linkedin.com/in/renzo-yaques-b36456253/" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
                 <li class="list-inline-item">
-                <a title="github" href="http://www.github.com" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="github" href="https://github.com/Sapitorico" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
 
               </ul>
@@ -64,7 +64,7 @@ const Team = () => {
             <a title="linkedin" href="http://www.linkedin.com" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
             </li>
             <li class="list-inline-item">
-            <a title="github" href="http://www.github.com" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+            <a title="github" href="https://github.com/JoaquinFurta" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
             </li>
 
               </ul>
@@ -87,10 +87,10 @@ const Team = () => {
               <ul class="list-inline">
 
                 <li class="list-inline-item">
-                <a title="linkedin" href="http://www.linkedin.com" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="linkedin" href="https://www.linkedin.com/in/mauricio-de-betolaza-del-puerto-24464a265/" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
                 <li class="list-inline-item">
-                <a title="github" href="http://www.github.com" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="github" href="https://github.com/Maurlcio" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
 
               </ul>
@@ -113,10 +113,10 @@ const Team = () => {
               <ul class="list-inline">
 
                 <li class="list-inline-item">
-                <a title="linkedin" href="http://www.linkedin.com" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="linkedin" href="https://www.linkedin.com/in/juan-rey-a34897255/" target="_blank" rel="noreferrer"><img src={ln} alt="Linkedin" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
                 <li class="list-inline-item">
-                <a title="github" href="http://www.github.com" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
+                <a title="github" href="https://github.com/fernan2lopezkto" target="_blank" rel="noreferrer"><img src={gh} alt="Github" style={{width: '30px'}} class="text-decoration-none d-block px-1" /></a>
                 </li>
 
               </ul>
