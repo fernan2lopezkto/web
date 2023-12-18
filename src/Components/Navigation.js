d16d7b0a9f7b65b9b23293cc417258fcb864ef20
JuanFRey <fernan2lopezkto@gmail.com>
2023-12-18 11:31:57 -0300

    fix link to header

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,13 +4,13 @@ import React from "react";
 const Navigation = () => {
     return (
         <>
-<section class="smart-scroll" style={{backgroundColor: "rgba(0,0,0,0.9)"}}>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-md navbar-dark">
-            <a class="navbar-brand heading-black" href="../templates/index.html">
+<section className="smart-scroll" style={{backgroundColor: "rgba(0,0,0,0.9)"}}>
+    <div className="container-fluid">
+        <nav className="navbar navbar-expand-md navbar-dark">
+            <a className="navbar-brand heading-black" href="#home">
                 HandSpeak
             </a>
-            <button class="navbar-toggler navbar-toggler-right border-0"
+            <button className="navbar-toggler navbar-toggler-right border-0"
                     type="button"
                     data-toggle="collapse"
                     data-target="#navbarCollapse"
@@ -19,16 +19,16 @@ const Navigation = () => {
                     aria-label="Toggle navigation">
                 <span data-feather="grid"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link page-scroll" href="#introduccion">Introducción</a>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link page-scroll" href="#introduccion">Introducción</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link page-scroll" href="#detalles">Detalles</a>
+                    <li className="nav-item">
+                        <a className="nav-link page-scroll" href="#detalles">Detalles</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link page-scroll" href="#PF">PF</a>
+                    <li className="nav-item">
+                        <a className="nav-link page-scroll" href="#PF">PF</a>
                     </li>
                 </ul>
             </div>
